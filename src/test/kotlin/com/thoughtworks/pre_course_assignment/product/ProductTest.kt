c860d7be090947e295ea5b3092ff5bea766dca59
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-19 17:00:51 +0800

    should be counted when given multiple products

--- src/test/kotlin/com/thoughtworks/pre_course_assignment/product/ProductTest.kt
+++ src/test/kotlin/com/thoughtworks/pre_course_assignment/product/ProductTest.kt
@@ -64,4 +64,28 @@ class ProductTest {
         assertEquals("kotlin", products[0].name)
         assertEquals(159, products[0].quantity)
     }
+
+    @Test
+    fun `should be counted when given multiple products`() {
+        val PRODUCT_1 = ProductDTO("kotlin", "SKU_1")
+        val PRODUCT_2 = ProductDTO("phone", "SKU_2")
+        val INVENTORIES = listOf(
+            InventoryDTO("SKU_1", "A", 13),
+            InventoryDTO("SKU_1", "B", 9),
+            InventoryDTO("SKU_2", "B", 1),
+            InventoryDTO("SKU_2", "C", 30),
+        )
+
+        every { mockedClient.listProducts() } returns listOf(PRODUCT_1, PRODUCT_2)
+        every { mockedClient.listInventories() } returns INVENTORIES
+
+        val products = this.productService.listProducts()
+
+        assertEquals(2, products.size)
+        assertEquals(1, products.filter { it.sku == "SKU_1" }.size)
+        assertEquals(1, products.filter { it.sku == "SKU_2" }.size)
+        assertEquals(22, products.first{ it.sku == "SKU_1"}.quantity)
+        assertEquals(31, products.first{ it.sku == "SKU_2"}.quantity)
+    }
+
 }
