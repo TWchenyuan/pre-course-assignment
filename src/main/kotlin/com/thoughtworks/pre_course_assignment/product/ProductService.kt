6e6c12c983c27a488a993e5342d4a0dfc2754214
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-19 16:51:32 +0800

    refactor list product

--- src/main/kotlin/com/thoughtworks/pre_course_assignment/product/ProductService.kt
+++ src/main/kotlin/com/thoughtworks/pre_course_assignment/product/ProductService.kt
@@ -3,17 +3,33 @@ package com.thoughtworks.pre_course_assignment.product
 class ProductService(private val client: ProductClient) {
     fun listProducts(): List<Product> {
         val products = client.listProducts()
-        val inventories = client.listInventories()
-        val productInventoryHash = inventories.groupBy { it.sku }
-        return products.map {
-            Product(it.sku, it.name, productInventoryHash.get(it.sku)?.map { it.quantity }?.sum() ?: 0)
-        }
+        val inventoryHash = client.listInventories().groupBy { it.sku }
+        return products.map { buildProduct(it, inventoryHash) }
     }
+
+    private fun buildProduct(product: ProductDTO, inventoryHash: Map<String, List<InventoryDTO>>): Product {
+        return product.toProduct(inventoryHash.getOrDefault(product.sku, emptyList()))
+    }
+}
+
+fun ProductDTO.toProduct(inventories: List<InventoryDTO>): Product {
+    return Product(
+        this.sku,
+        this.name,
+        inventories.map { Product.Inventory(it.zone, it.quantity) }
+    )
 }
 
 data class Product(
     val sku: String,
     val name: String,
-    val quantity: Long
-)
+    private val inventories: List<Inventory>,
+) {
 
+    val quantity: Long = inventories.sumOf { it.quantity }
+
+    data class Inventory(
+        val zone: String,
+        val quantity: Long
+    )
+}
