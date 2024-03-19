d08392b3c94bf414fbf50f51d52b21e208910b93
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-19 15:18:38 +0800

    should print table when give a product

---
+++ src/test/kotlin/com/thoughtworks/pre_course_assignment/PrintUtilTest.kt
@@ -0,0 +1,41 @@
+package com.thoughtworks.pre_course_assignment
+
+import com.thoughtworks.pre_course_assginment.util.PrintUtil
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import kotlin.test.assertEquals
+
+class PrintUtilTest {
+    private val util = PrintUtil()
+    private val outputStreamCaptor = ByteArrayOutputStream()
+    private val originalOut = System.out
+
+    @BeforeEach
+    fun setup() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+    @AfterEach
+    fun teardown() {
+        System.setOut(originalOut)
+    }
+
+    @Test
+    fun `should print table when give a product`() {
+        val headers = listOf("SKU", "name", "price", "quantity", "image")
+        val product = listOf("SKU_1", "Watch", "2999.0", "1000", "image.jpg")
+        util.printTable(headers, listOf(product))
+        assertEquals(
+            """
++-------+-------+--------+----------+-----------+
+| SKU   | name  | price  | quantity | image     |
++-------+-------+--------+----------+-----------+
+| SKU_1 | Watch | 2999.0 | 1000     | image.jpg |
++-------+-------+--------+----------+-----------+
+        """.trimIndent(), outputStreamCaptor.toString().trim()
+        )
+    }
+}
